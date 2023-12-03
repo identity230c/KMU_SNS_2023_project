1b50098f4575c898b91edb37cb21bc4a00f191fd
Lee jongsik <identity230c@kookmin.ac.kr>
2023-12-03 17:18:05 +0900

    [fix] ipconfig

--- utils/ipconifg.py
+++ utils/ipconifg.py
@@ -1,12 +1,25 @@
 import os
 
 def executeIpConfig():
-    os.system(f"ipconfig /all | findstr 물리적> ./ipconfig.txt")
+    os.system(f"ipconfig /all | findstr /i /c:어댑터 /c:물리적 /c:미디어> ./ipconfig.txt")
     ret = ""
     with open("ipconfig.txt", 'r') as f:
         ret = f.readlines()
-    ret = ret[0]
-    ret = ret.split(":")[1][1:-1]
+    connect = True
+    adap = ""
+    addr = ""
+    for i in ret:
+        tmp = i.strip()
+        if "어댑터" in tmp:
+            adap = tmp[:-1]
+            connect = True
+        elif "미디어" in tmp:
+            connect = False
+        elif "물리적" in tmp:
+            if connect:
+                addr = tmp.split(" : ")[-1]
+                break
+    ret = f"{addr}({adap})"
     return ret
 
 if __name__ == "__main__":
